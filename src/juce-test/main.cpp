9b24c5a5612761d28758a3ff25c17d2d9871787b
Christian Zimmer <christian@zimmer-bw.de>
2014-04-01 18:09:52 +0200

    kleines Beispiel in Juce

--- src/juce-test/main.cpp
+++ src/juce-test/main.cpp
@@ -1,11 +1,18 @@
 #include <iostream>
 #include "juce_audio_devices.h"
+#include <objbase.h>
+
 using namespace std;
 using namespace juce;
 
 int main()
 {
-	AudioDeviceManager adm();
+
+	CoInitialize(NULL);
+	AudioDeviceManager* adm = new AudioDeviceManager();
+	adm->initialiseWithDefaultDevices(1, 0);
+	
+
 	cout << "Hallo Welt" << endl;
 }
 
