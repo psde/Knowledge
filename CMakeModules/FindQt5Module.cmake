1d13376d835252e215d73ff1198dd21eccb84fb2
Mathias Garbe <mail@mathias-garbe.de>
2014-04-02 21:07:19 +0200

    Checks if Qt$module_DIR ist defined and only then sets a new one.
    Signed-off-by: Mathias Garbe <mail@mathias-garbe.de>

--- CMakeModules/FindQt5Module.cmake
+++ CMakeModules/FindQt5Module.cmake
@@ -1,7 +1,10 @@
 # Wtf ...
 function(FindQt5Module module)
-	set("Qt5${module}_DIR" "${Qt_ROOT}/qtbase/lib/cmake/Qt5${module}")
-	set("Qt5${module}_DIR" "${Qt_ROOT}/qtbase/lib/cmake/Qt5${module}" PARENT_SCOPE)
+	if(NOT DEFINED "Qt5${module}_DIR") 
+		set("Qt5${module}_DIR" "${Qt_ROOT}/qtbase/lib/cmake/Qt5${module}")
+		set("Qt5${module}_DIR" "${Qt_ROOT}/qtbase/lib/cmake/Qt5${module}" PARENT_SCOPE)
+	endif()
+
 	find_package("Qt5${module}" REQUIRED)
 	message(STATUS "Found Qt5${module} config in ${Qt5${module}_DIR}")
 endfunction()
