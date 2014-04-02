d61c1bd9ad8e8d2307d0a7883d98bfda945437a1
Mathias Garbe <me@mathias-garbe.de>
2014-04-02 21:23:19 +0200

    Fixex linux compliation

--- CMakeModules/FindQt5Module.cmake
+++ CMakeModules/FindQt5Module.cmake
@@ -1,8 +1,8 @@
 # Wtf ...
 function(FindQt5Module module)
-	if(NOT DEFINED "Qt5${module}_DIR") 
+	if(DEFINED "${Qt_ROOT}")
 		set("Qt5${module}_DIR" "${Qt_ROOT}/qtbase/lib/cmake/Qt5${module}")
-		set("Qt5${module}_DIR" "${Qt_ROOT}/qtbase/lib/cmake/Qt5${module}" PARENT_SCOPE)
+		set("Qt5${module}_DIR" "${Qt5${module}_DIR}" PARENT_SCOPE)
 	endif()
 
 	find_package("Qt5${module}" REQUIRED)
