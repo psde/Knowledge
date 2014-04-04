0d95ca94c31dd49a68f199c112ac0391617503c7
Mathias Garbe <me@mathias-garbe.de>
2014-04-04 10:24:49 +0200

    openct-test now actually opens the camera

--- src/opencv-test/main.cpp
+++ src/opencv-test/main.cpp
@@ -2,8 +2,26 @@
 #include <opencv2/opencv.hpp>
 
 int main( int argc, char** argv )
-{
-	cv::namedWindow("Named Window", CV_WINDOW_AUTOSIZE);
-	cv::waitKey(0);
+{       
+    cv::VideoCapture cap(0);
+
+    if (!cap.isOpened())
+    {
+        std::cout << "Not initialized?" << std::endl;
+        return -1;
+    }
+
+	cv::namedWindow("Camera", CV_WINDOW_AUTOSIZE);
+
+    while(true)
+    {
+        cv::Mat frame;
+        cap >> frame;
+        cv::imshow("Camera", frame);
+
+        int key = cv::waitKey(1);   
+        if(key > 0)
+            break;
+    }
 	return 0;
 }
